3a7033c1680afd0e8dce7fa26f8d05b2f03071bf
sravi12 <sravi12@users.noreply.github.com>
2020-10-07 15:11:34 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace demo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World from Azure 2!");
+                await context.Response.WriteAsync("Hello World from Azure in DAP demo!");
             });
         }
     }
